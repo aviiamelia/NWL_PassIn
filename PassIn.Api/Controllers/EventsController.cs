0ac28dc0e1b1b334f94a16e941a926b61d06c55b
rafael <rafael.zoe26@gmail.com>
2024-04-02 04:51:49 -0300

    feat: controller structure

--- PassIn.Api/Controllers/EventsController.cs
+++ PassIn.Api/Controllers/EventsController.cs
@@ -2,14 +2,33 @@
 
 namespace PassIn.Api.Controllers;
 
+using PassIn.Application.UseCases.Events.Register;
+using PassIn.Communication.Requests;
+using PassIn.Communication.Responses;
+
 [Route("api/[controller]")]
 [ApiController]
 public class EventsController : ControllerBase
 {
     [HttpPost]
-
-    public IActionResult Register()
+    [ProducesResponseType(typeof(ResponseRegisterEventJsoncs),StatusCodes.Status201Created)]
+    [ProducesResponseType(typeof(ResponseErrorJson), StatusCodes.Status400BadRequest)]
+    public IActionResult Register([FromBody] RequestEventJson request)
     {
-        return Created();
+        try
+        {
+            var useCase = new RegisterEventUseCase();
+            useCase.Execute(request);
+
+            return Created();
+        }
+        catch (ArgumentException ex)
+        {
+            return BadRequest(new ResponseErrorJson(ex.Message));
+        }
+        catch
+        {
+            return StatusCode(StatusCodes.Status500InternalServerError, new ResponseErrorJson("Unknown error"));
+        }
     }
 }
